b2e597e9d04374eed4f81f42401afb884819b3c2
JrCorrea77 <jrcorrea.bsb@gmail.com>
2026-01-16 17:45:12 +0000

    correção de erro

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 function Home(){
     return <h1> Página em construção. Será a minha primeira página na web!</h1>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Em Construção</title>
-    <style></style>
-
+    
 }
 
 export default Home;
