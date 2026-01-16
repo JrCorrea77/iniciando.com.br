38e9b5edd9d77fd834592b396283ccf00583d71d
JrCorrea77 <jrcorrea.bsb@gmail.com>
2026-01-16 17:43:11 +0000

    adicionando novos itens na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 function Home(){
     return <h1> Página em construção. Será a minha primeira página na web!</h1>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Em Construção</title>
+    <style></style>
+
 }
 
 export default Home;
