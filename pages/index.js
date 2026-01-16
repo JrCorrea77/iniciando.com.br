0bb57ede59e97e9f2426c1f138c7eb79e69c1541
JrCorrea77 <jrcorrea.bsb@gmail.com>
2026-01-16 17:12:11 +0000

    Segunda modificação da frase de construção do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Página em construção. Agradecemos a paciência! EM BREVE TEREMOS NOVIDADES!</h1>
+    return <h1> Página em construção. Agradecemos a paciência! EM BREVE TEREMOS NOVIDADES BOAS QUE ESPERO ATENDER AS NECESSIDADES DE TODOS OS USUÁRIOS!</h1>
 }
 
 export default Home;
