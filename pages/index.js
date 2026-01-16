fcb6307acce2516a6dc7d30a4f5d78b357fb0d6e
JrCorrea77 <jrcorrea.bsb@gmail.com>
2026-01-16 16:55:22 +0000

    modifica a frase da página de construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Página em construção. Agradecemos a paciência!</h1>
+    return <h1> Página em construção. Agradecemos a paciência! EM BREVE TEREMOS NOVIDADES!</h1>
 }
 
 export default Home;
